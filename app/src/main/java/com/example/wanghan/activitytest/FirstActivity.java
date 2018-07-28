d3fc849814b3e79e7de9efdda0477a8b67f8db2a
hustacer <xiaobinly@gmail.com>
2018-07-28 14:37:20 +0900

    11

---
+++ app/src/main/java/com/example/wanghan/activitytest/FirstActivity.java
@@ -0,0 +1,64 @@
+package com.example.wanghan.activitytest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+import android.widget.Toast;
+import android.util.Log;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You click Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return  true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String returnData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnData);
+                }
+                break;
+                default:
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.activitytest.ACTION_START");
+                startActivityForResult(intent, 1);
+                //startActivity(intent);
+            }
+        });
+    }
+}
